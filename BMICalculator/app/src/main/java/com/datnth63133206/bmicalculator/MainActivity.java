6ce426a631889aeaeb810ba7a42ed1b8604f72f4
NgoThanhDat_63133206 <dat.nth.63cntt@ntu.edu.vn>
2023-10-04 13:10:38 +0700

    Update bmi

---
+++ BMICalculator/app/src/main/java/com/datnth63133206/bmicalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.datnth63133206.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
