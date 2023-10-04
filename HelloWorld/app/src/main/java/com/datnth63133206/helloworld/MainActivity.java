8dd7f89dc1d732e63c8ea6d090bb3b09cd2993b8
NgoThanhDat_63133206 <dat.nth.63cntt@ntu.edu.vn>
2023-10-04 14:16:27 +0700

    Update Helloworld with listener

--- HelloWorld/app/src/main/java/com/datnth63133206/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/datnth63133206/helloworld/MainActivity.java
@@ -3,12 +3,26 @@ package com.datnth63133206.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    public void btnHello_click(View v){
+        String strThongBao = "Hello world";
+        Toast.makeText(this,strThongBao,Toast.LENGTH_LONG).show();
+    }
+    public void btnName_click(View v){
+        EditText etName = findViewById(R.id.etName);
+        String strThongBao = "Hello " + etName.getText()+"!";
+        Toast.makeText(this,strThongBao,Toast.LENGTH_LONG).show();
+    }
+
 }
